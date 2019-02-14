36db8b2843f73c78afde1eabdbddc39be7df633f
BhavyaKashetty <42703264+BhavyaKashetty@users.noreply.github.com>
2019-02-14 15:07:51 -0600

    Add files via upload

---
+++ DFS.java
@@ -0,0 +1,137 @@
+/** Starter code for SP8
+ *  Karan Thakkar - krt170130
+ *  Bhavya Kashetty - bxk180012
+ */
+
+package bxk180012;
+
+import rbk.Graph;
+import rbk.Graph.Vertex;
+import rbk.Graph.Edge;
+import rbk.Graph.GraphAlgorithm;
+import rbk.Graph.Factory;
+import rbk.Graph.Timer;
+
+import java.io.File;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class DFS extends GraphAlgorithm<DFS.DFSVertex> {
+    enum color{
+        WHITE,BLACK,GREY;
+    }
+    boolean isCyclic;
+    int topNum;
+    LinkedList<Vertex> finishList;
+    public static class DFSVertex implements Factory {
+        int cno;
+        Vertex parent;
+        color nodeColor;
+        int top;
+        DFSVertex(){
+
+        }
+        public DFSVertex(Vertex u) {
+            this.cno = 0;
+            this.parent = null;
+            this.nodeColor = color.WHITE;
+            this.top = 0;
+        }
+        public DFSVertex make(Vertex u) { return new DFSVertex(u); }
+    }
+
+    DFSVertex d = new DFSVertex();
+    public DFS(Graph g) {
+        super(g, new DFSVertex(null));
+        this.finishList = new LinkedList<>();
+        this.isCyclic = false;
+        this.topNum = g.size();
+    }
+
+    public static DFS depthFirstSearch(Graph g) {
+        DFS d = new DFS(g);
+        for (Vertex u: g) {
+            if(d.get(u).nodeColor == color.WHITE){
+                d.dfsVisit(u);
+            }
+        }
+        return d;
+    }
+
+    // Member function to find topological order
+    public List<Vertex> topologicalOrder1() {
+        DFS d = DFS.depthFirstSearch(this.g);
+        return d.isCyclic ? null : d.finishList;
+    }
+    void dfsVisit(Vertex u){
+        get(u).nodeColor = color.GREY;
+        for(Edge e:g.outEdges(u)) {
+            Vertex d2 = e.toVertex();
+            if(get(d2).nodeColor == color.GREY) {
+                this.isCyclic = true;
+                return;
+            }
+            else if(get(d2).nodeColor == color.WHITE) {
+                get(d2).parent = u;
+                dfsVisit(e.toVertex());
+                if(this.isCyclic)return;
+            }
+        }
+        get(u).nodeColor = color.BLACK;
+        get(u).top = this.topNum--;
+        this.finishList.addFirst(u);
+    }
+
+
+
+    // Find the number of connected components of the graph g by running dfs.
+    // Enter the component number of each vertex u in u.cno.
+    // Note that the graph g is available as a class field via GraphAlgorithm.
+    public int connectedComponents() {
+        return 0;
+    }
+
+    // After running the connected components algorithm, the component no of each vertex can be queried.
+    public int cno(Vertex u) {
+        return get(u).cno;
+    }
+
+    // Find topological oder of a DAG using DFS. Returns null if g is not a DAG.
+    public static List<Vertex> topologicalOrder1(Graph g) {
+        if(!g.isDirected()) return null;
+        DFS d = new DFS(g);
+        return d.topologicalOrder1();
+    }
+
+    // Find topological oder of a DAG using the second algorithm. Returns null if g is not a DAG.
+    public static List<Vertex> topologicalOrder2(Graph g) {
+        return null;
+    }
+
+//    public static void main(String[] args) throws Exception {
+////        String string = "7 8   1 2 2   1 3 3   2 4 5   3 4 4   4 5 1   5 1 7   6 7 1   7 6 1 0";
+////	     String string = "2 2   1 2 2   2 1 2 0";
+////        String string = "2 0 0";
+//        String string = "5 5   1 2 2   1 3 3   2 4 5   3 4 4   4 5 1 0";
+//        Scanner in;
+//        // If there is a command line argument, use it as file from which
+//        // input is read, otherwise use input from string.
+//        in = args.length > 0 ? new Scanner(new File(args[0])) : new Scanner(string);
+//
+//        // Read graph from input
+//        Graph g = Graph.readGraph(in,true);
+//        g.printGraph(true);
+//
+//        List <Vertex> result = DFS.topologicalOrder1(g);
+//        if(result == null)
+//            System.out.println("Graph is not DAG");
+//        else
+//            System.out.println(result);
+//        /*int numcc = d.connectedComponents();
+//        System.out.println("Number of components: " + numcc + "\nu\tcno");
+//        for(Vertex u: g) {
+//            System.out.println(u + "\t" + d.cno(u));
+//        }*/
+//    }
+}
---
+++ EnumerateTopological.java
@@ -0,0 +1,165 @@
+/* Starter code for enumerating topological orders of a DAG
+ * @author
+ */
+
+package bxk180012;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+import rbk.Graph;
+import rbk.Graph.GraphAlgorithm;
+import rbk.Graph.Timer;
+import rbk.Graph.Vertex;
+import rbk.Graph.Edge;
+import rbk.Graph.Factory;
+
+public class EnumerateTopological extends GraphAlgorithm<EnumerateTopological.EnumVertex> {
+    boolean print;  // Set to true to print array in visit
+    long count;      // Number of permutations or combinations visited
+    Selector sel;
+    ArrayList<String> vistedPaths = new ArrayList<>();
+    HashSet<String> h = new HashSet<String>(); 
+    public EnumerateTopological(Graph g) {
+	super(g, new EnumVertex());
+	print = false;
+	count = 0;
+	sel = new Selector();
+    }
+
+    static class EnumVertex implements Factory {
+    	int indegree;
+    	boolean visited;
+	EnumVertex() { }
+	EnumVertex(Vertex u){
+		indegree = 0;
+		visited = false;
+	}
+	public EnumVertex make(Vertex u) { return new EnumVertex();	}
+    }
+
+    class Selector extends Enumerate.Approver<Vertex> {
+	@Override
+	public boolean select(Vertex u) {
+	    return true;
+	}
+
+	@Override
+	public void unselect(Vertex u) {
+	}
+
+	@Override
+	public void visit(Vertex[] arr, int k) {
+	    count++;
+	    if(print) {
+		for(Vertex u: arr) {
+		    System.out.print(u + " ");
+		}
+		System.out.println();
+	    }
+	}
+    } 
+    
+  
+    void enumerationPath(Vertex v,int i,Vertex[] arr) {
+    	arr[i] = v;
+		//if the number of nodes visited is equal to the size of array,
+		//calling the visit method
+    	if(i == g.size()-1) {
+    		String path = Arrays.toString(arr);
+    		if (h.contains(path) == false) {
+    			h.add(path);
+    			sel.visit(arr, g.size());
+    		}
+    	} else {
+    	   	//Copying the visited nodes into another array
+        	Vertex[] visitedNodes = new Vertex[arr.length];		
+    		System.arraycopy(arr, 0, visitedNodes, 0, arr.length);
+    		//For the node that is visited, 
+    		//decreasing the indegree of the nodes to the other edges
+    		for(Edge e:g.outEdges(v)) {
+    			Vertex w = e.toVertex();
+    			if(get(w).indegree > 0) get(w).indegree = get(w).indegree - 1;
+    		}
+    		int[] indegress = new int[arr.length];
+    		int a=0;
+    		//Nodes which have indegree as zero, making them as next nodes visited that have to visited
+    		ArrayDeque<Vertex> zeroq = new ArrayDeque<>();
+    		for(Vertex x:g) {
+    			if(get(x).indegree == 0) {
+    				if(Arrays.asList(arr).contains(x) == false) zeroq.add(x);
+    			}
+    			indegress[a] = get(x).indegree ;
+    			a++;
+    		}
+    		int runWhileloop = zeroq.size();
+    		int p = i;
+    		while(runWhileloop>0) {
+    			for(Vertex j:zeroq) enumerationPath(j, ++i, arr);
+    			System.arraycopy(visitedNodes, 0, arr, 0, arr.length);
+    			a = 0;
+    			for(Vertex q:g) {
+    				if(Arrays.asList(arr).contains(q) == false) get(q).indegree = indegress[a];					 
+    				a++;
+    			}
+    			//once a path is visited, restoring the original values to visit the next path
+    			i = p;
+    			Vertex y = zeroq.removeFirst();
+    			zeroq.addLast(y);
+    			runWhileloop--;
+    		}
+    	}
+    }
+    
+       
+    
+    // To do: LP4; return the number of topological orders of g
+    public long enumerateTopological(boolean flag) {
+    	print = flag;
+     	for(Vertex u:g) {
+    		get(u).indegree = u.inDegree();
+    	}
+    	//for all the vertices which have indegree as zero making them as first visiting node
+    	//Continues for each node which has indegree as zero
+    	for(Vertex v:g) {
+    		if(v.inDegree() == 0) {
+    			Vertex[] arr = new Vertex[g.size()];
+    			enumerationPath(v, 0, arr);
+    	   		//Once all the paths are visited with one starting node
+        		//Restoring the indegrees of all the nodes for the visits on the node 
+    			for(Vertex w:g) {
+    	    		get(w).indegree = w.inDegree();
+    	    	}
+      		}
+    	}
+    	return count;
+    }
+
+    //-------------------static methods----------------------
+
+    public static long countTopologicalOrders(Graph g) {
+        EnumerateTopological et = new EnumerateTopological(g);
+	return et.enumerateTopological(false);
+    }
+
+    public static long enumerateTopologicalOrders(Graph g) {
+        EnumerateTopological et = new EnumerateTopological(g);
+	return et.enumerateTopological(true);
+    }
+
+    public static void main(String[] args) {
+	int VERBOSE = 0;
+        if(args.length > 0) { VERBOSE = Integer.parseInt(args[0]); }
+        Graph g = Graph.readDirectedGraph(new java.util.Scanner(System.in));
+        Graph.Timer t = new Graph.Timer();
+        long result;
+	if(VERBOSE > 0) {
+	    result = enumerateTopologicalOrders(g);
+	} else {
+	    result = countTopologicalOrders(g);
+	}
+        System.out.println("\n" + result + "\n" + t.end());
+    }
+
+}
